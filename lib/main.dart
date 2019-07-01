d2dd9a90836a687505406e2d21e8e450667ee500
Federico Creti <federico.creti87@gmail.com>
2019-07-01 18:46:16 +0200

    Passed data to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Course App'),
       ),
-      body: ProductManager(),
+      body: ProductManager('Food Tester'),
     ));
   }
 }
